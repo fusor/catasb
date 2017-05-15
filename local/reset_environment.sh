6a2175c95670ad184f723fc754ebba210666e8d6
Christopher Chase <cfc@chasenc.com>
2017-05-15 14:48:29 -0400

    Added docker_org_name. (#45)
    Set reset variable based on script.
    Removed some unneeded demo specific vars.
    Added source of my_vars for user specific variables.

--- local/reset_environment.sh
+++ local/reset_environment.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+export RESET_ENV="True"
 
 ANS_CODE="../ansible"
+source ./my_vars
 source ./common_vars
 ansible-playbook ${ANS_CODE}/reset_local_environment.yml --extra-vars "${EXTRA_VARS}"
--- local/run_setup_local.sh
+++ local/run_setup_local.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+export RESET_ENV="False"
 
 ANS_CODE="../ansible"
+source ./my_vars
 source ./common_vars
 ansible-playbook ${ANS_CODE}/setup_local_environment.yml --extra-vars "${EXTRA_VARS}"
