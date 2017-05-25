c06c78d53a2dae61bfed99bf2c0b4c82b51ff9ea
Fabian von Feilitzsch <fabianvf@users.noreply.github.com>
2017-05-25 14:31:41 -0400

    local setup prompts (#51)
    * Add prompts for dockerhub credentials, make my_vars file optional, add support for arbitrary ansible arguments when executing local setup
    
    * update reset_environmnent, add support for my_vars.yml

--- local/reset_environment.sh
+++ local/reset_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 export RESET_ENV="True"
 
+extra_args=''
 ANS_CODE="../ansible"
-source ./my_vars
 source ./common_vars
-ansible-playbook ${ANS_CODE}/reset_local_environment.yml --extra-vars "${EXTRA_VARS}"
+[[ ! -e my_vars.yml ]] || extra_args='-e @my_vars.yml'
+ansible-playbook ${ANS_CODE}/reset_local_environment.yml --extra-vars "${EXTRA_VARS}" $extra_args $@
