b09117433ba0e625680f461b85a1a510b1865e6f
Ryan Hallisey <rhallise@redhat.com>
2017-08-04 10:37:58 -0400

    Fix misnamed playbook for the gate (#108)
    In an early patch, the gate had its own playbook called
    setup-gate-environment.yml, but it was combined with
    setup-local-environment.yml.

--- local/gate/reset_environment.sh
+++ local/gate/reset_environment.sh
@@ -2,4 +2,4 @@
 
 source ../../gather_config
 
-ansible-playbook ${ANS_CODE}/setup_gate_environment.yml --extra-vars "${EXTRA_VARS}" ${extra_args} $@
+ansible-playbook ${ANS_CODE}/setup_local_environment.yml --extra-vars "${EXTRA_VARS}" ${extra_args} $@
