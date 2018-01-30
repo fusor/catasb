fa6a06820d4bc2664493417059cf41ac95003fb8
jkim <johnkim76@gmail.com>
2018-01-29 19:11:11 -0500

    Refactored the AWS EC2 minimal roles to prep for the multi node feature

--- ec2/minimal/run_create_infrastructure.sh
+++ ec2/minimal/run_create_infrastructure.sh
@@ -4,11 +4,13 @@
 [ -z "$AWS_SSH_PRIV_KEY_PATH" ] && echo -e "Missing environment variable:  AWS_SSH_PRIV_KEY_PATH\nPlease set this to the path for your SSH private key" && exit 1;
 [ ! -r "$AWS_SSH_PRIV_KEY_PATH" ] && echo -e "Unable to read file pointed to by, AWS_SSH_PRIV_KEY_PATH, $AWS_SSH_PRIV_KEY_PATH" && exit 1;
 
+EC2_TYPE="minimal"
+
 source ../../gather_config
 
 ansible-playbook \
   -u ${EC2_USER} \
   --private-key ${AWS_SSH_PRIV_KEY_PATH} \
-  ${ANS_CODE}/infrastructure.yml \
+  ${ANS_CODE}/setup_aws_infrastructure.yml \
   --extra-vars "${EXTRA_VARS}" \
   ${extra_args} $@
--- ec2/minimal/run_setup_environment.sh
+++ ec2/minimal/run_setup_environment.sh
@@ -4,12 +4,14 @@
 [ -z "$AWS_SSH_PRIV_KEY_PATH" ] && echo "Missing environment variable:  AWS_SSH_PRIV_KEY_PATH\nPlease set this to the path for your SSH private key\n" && exit 1;
 [ ! -r "$AWS_SSH_PRIV_KEY_PATH" ] && echo -e "Unable to read file pointed to by, AWS_SSH_PRIV_KEY_PATH, $AWS_SSH_PRIV_KEY_PATH" && exit 1;
 
+EC2_TYPE="minimal"
+
 source ../../gather_config
 
 ansible-playbook \
   -u ${EC2_USER} \
   --private-key ${AWS_SSH_PRIV_KEY_PATH} \
   -i ${ANS_CODE}/library/inventory/ec2.py \
-  ${ANS_CODE}/setup_environment.yml \
+  ${ANS_CODE}/setup_aws_environment.yml \
   --extra-vars "${EXTRA_VARS}" \
   ${extra_args} $@
